f9bf409a515b61152d0f3e7bbed50ad871d0b5b3
giovsteph <giov.ponce@gmail.com>
2019-11-06 08:42:48 -0600

    se agrega funcion window.cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,65 +1,66 @@
 //funcion para codificar
+window.cipher = {
+    let result = "";
+    const cipher = () => {
+        document.getElementById("msgToCipherScrn").style.display = "none";
+        document.getElementById("cipheredMsgScrn").style.display = "block";
 
-let result = "";
-const cipher = () => {
-    document.getElementById("msgToCipherScrn").style.display = "none";
-    document.getElementById("cipheredMsgScrn").style.display = "block";
+        let shift = document.getElementById("shiftNmbr").value;
+        let text = document.getElementById("msgToCode").value;
+        let shift1 = Number(shift);
+        let text1 = String(text);
 
-    let shift = document.getElementById("shiftNmbr").value;
-    let text = document.getElementById("msgToCode").value;
-    let shift1 = Number(shift);
-    let text1 = String(text);
+        for (let i = 0; i < text1.length; i++) {
+            if (text1[i] == text1[i].toUpperCase()) {
+                let a = text1[i].charCodeAt(0);
+                let e = (((a - 65 + shift1) % 26) + 65);
+                result = result + String.fromCharCode(e).toUpperCase();
+            } else if (text1[i] == text1[i].toLowerCase()) {
+                let a = text1[i].charCodeAt(0);
+                let e = (((a - 97 + shift1) % 26) + 97);
+                result = result + String.fromCharCode(e);
+            } else {
+                result = result + text1[i];
+            }
 
-    for (let i = 0; i < text1.length; i++) {
-        if (text1[i] == text1[i].toUpperCase()) {
-            let a = text1[i].charCodeAt(0);
-            let e = (((a - 65 + shift1) % 26) + 65);
-            result = result + String.fromCharCode(e).toUpperCase();
-        } else if (text1[i] == text1[i].toLowerCase()) {
-            let a = text1[i].charCodeAt(0);
-            let e = (((a - 97 + shift1) % 26) + 97);
-            result = result + String.fromCharCode(e);
-        } else {
-            result = result + text1[i];
         }
-
-    }
-    console.log(result);
-    document.getElementById("transmission").innerHTML = result;
-}
-
-document.getElementById("cipherBegin").addEventListener("click", cipher);
-
-
-
-//funcion para decodificar
-
-let resultDec = "";
-const decipher = () => {
-    document.getElementById("msgToDecipherScrn").style.display = "none";
-    document.getElementById("decipheredMsgScrn").style.display = "block";
-
-    let shiftDec = document.getElementById("shiftNmbrDec").value;
-    let textDec = document.getElementById("msgToDecode").value;
-    let shiftDec1 = Number(shiftDec);
-    let textDec1 = String(textDec);
-
-    for (let i = 0; i < textDec1.length; i++) {
-        if (textDec1[i] == textDec1[i].toUpperCase()) {
-            let a = textDec1[i].charCodeAt(0);
-            let e = (((a - 65 - shiftDec1) % 26) + 65);
-            resultDec = resultDec + String.fromCharCode(e).toUpperCase();
-        } else if (textDec1[i] == textDec1[i].toLowerCase()) {
-            let a = textDec1[i].charCodeAt(0);
-            let e = (((a - 97 - shiftDec1) % 26) + 97);
-            resultDec = resultDec + String.fromCharCode(e);
-        } else {
-            resultDec = resultDec + textDec1[i];
-        }
-
+        console.log(result);
+        document.getElementById("transmission").innerHTML = result;
     }
 
-    document.getElementById("transmissionDec").innerHTML = resultDec;
-}
+    document.getElementById("cipherBegin").addEventListener("click", cipher);
 
-document.getElementById("decipherBegin").addEventListener("click", decipher);
+
+
+    //funcion para decodificar
+
+    let resultDec = "";
+    const decipher = () => {
+        document.getElementById("msgToDecipherScrn").style.display = "none";
+        document.getElementById("decipheredMsgScrn").style.display = "block";
+
+        let shiftDec = document.getElementById("shiftNmbrDec").value;
+        let textDec = document.getElementById("msgToDecode").value;
+        let shiftDec1 = Number(shiftDec);
+        let textDec1 = String(textDec);
+
+        for (let i = 0; i < textDec1.length; i++) {
+            if (textDec1[i] == textDec1[i].toUpperCase()) {
+                let a = textDec1[i].charCodeAt(0);
+                let e = (((a - 65 - shiftDec1) % 26) + 65);
+                resultDec = resultDec + String.fromCharCode(e).toUpperCase();
+            } else if (textDec1[i] == textDec1[i].toLowerCase()) {
+                let a = textDec1[i].charCodeAt(0);
+                let e = (((a - 97 - shiftDec1) % 26) + 97);
+                resultDec = resultDec + String.fromCharCode(e);
+            } else {
+                resultDec = resultDec + textDec1[i];
+            }
+
+        }
+
+        document.getElementById("transmissionDec").innerHTML = resultDec;
+    }
+
+    document.getElementById("decipherBegin").addEventListener("click", decipher);
+}
